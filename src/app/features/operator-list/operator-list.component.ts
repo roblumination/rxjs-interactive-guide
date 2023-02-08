e4e0755a3a09e1e3aa02bf99eac1c7ddf1c40532
Roman Savran <roman.savran@chisw.com>
2023-02-08 17:25:50 +0200

    fix blur on nav menu in mobile mode

--- src/app/features/operator-list/operator-list.component.ts
+++ src/app/features/operator-list/operator-list.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { OperatorNavigationService } from './components/operator-navigation/services/operator-navigation.service';
 
 @Component({
   selector: 'app-operator-list',
   templateUrl: './operator-list.component.html',
   styleUrls: ['./operator-list.component.scss'],
 })
-export class OperatorListComponent {}
+export class OperatorListComponent {
+  constructor(private navService: OperatorNavigationService) {}
+
+  public open() {
+    this.navService.openNavigation();
+  }
+}
